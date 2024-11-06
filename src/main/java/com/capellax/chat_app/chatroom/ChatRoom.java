23142c39754c5dd75c33e078b47e42c1f6541e29
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-06 16:51:24 +0300

    ChatRoom business logic implementation - Chat logic starter

--- src/main/java/com/capellax/chat_app/chatroom/ChatRoom.java
+++ src/main/java/com/capellax/chat_app/chatroom/ChatRoom.java
@@ -1,4 +1,25 @@
 package com.capellax.chat_app.chatroom;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
 public class ChatRoom {
+
+    @Id
+    private String id;
+
+    private String chatId;
+
+    private String senderId;
+
+    private String recipientId;
+
+
 }
---
+++ src/main/java/com/capellax/chat_app/chatroom/ChatRoomRepository.java
@@ -0,0 +1,11 @@
+package com.capellax.chat_app.chatroom;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+
+}
