9dcd92b3c53fe8dd3df14e329e85d06f8d555a48
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-06 17:00:46 +0300

    ChatMessage document, repository, and service layer implemented - Chat Controller layer starter

---
+++ src/main/java/com/capellax/chat_app/chat/ChatMessageService.java
@@ -0,0 +1,46 @@
+package com.capellax.chat_app.chat;
+
+import com.capellax.chat_app.chatroom.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(
+            ChatMessage chatMessage
+    ) {
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getChatId(),
+                chatMessage.getRecipientId(),
+                true
+        ).orElseThrow(() -> new RuntimeException("Chat room not found"));
+
+        chatMessage.setChatId(chatId);
+        chatMessageRepository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(
+            String senderId,
+            String recipientId
+    ) {
+        var chatId = chatRoomService.getChatRoomId(
+                senderId,
+                recipientId,
+                false
+        );
+
+        return chatId
+                .map(chatMessageRepository::findByChatId)
+                .orElse(new ArrayList<>());
+    }
+
+}
